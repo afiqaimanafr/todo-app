127ec30402857501ce0206b8d40f3f3fc5003711
afiqaimanafr <aaafr98@gmail.com>
2022-02-21 16:44:32 +0800

    latest ui

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import FirstComponent from './components/learning-examples/FirstComponent';
- 
+import React, { Component } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import FirstComponent from "./components/learning-examples/FirstComponent";
+import SecondComponent from "./components/learning-examples/SecondComponent";
+import Counter from "./components/counter/Counter";
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Todo App!
-        <FirstComponent/>
+        <Counter />
       </div>
     );
   }
 }
+
+class LearningComponents extends Component {
+  render() {
+    return (
+      <div className="LearningComponents">
+        Todo App!
+        <FirstComponent />
+        <SecondComponent />
+      </div>
+    );
+  }
+}
+
 export default App;
