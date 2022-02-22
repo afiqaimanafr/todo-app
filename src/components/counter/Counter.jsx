987d95d33ce4773d4e0477383ae5e97d08f48212
afiqaimanafr <aaafr98@gmail.com>
2022-02-22 15:42:04 +0800

    latest update

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,12 +1,35 @@
-import React from "react";
-import "./Counter.css"
+import React, { Component } from "react";
+import "./Counter.css";
 
+class Counter extends Component {
+  //Define the initial state in a constructor
+  //state => counter 0
 
-function Counter() {
-  return <div className="counter">
-      <button>+1</button>
-      <span className="count">0</span>
-  </div>;
+  constructor() {
+    super();
+    this.state = {
+      counter: 0,
+    };
+
+    this.increment = this.increment.bind(this);
+  }
+
+  render() {
+    return (
+      <div className="counter">
+        <button onClick={this.increment}>+1</button>
+        <span className="count">{this.state.counter}</span>
+      </div>
+    );
+  }
+
+  increment() {
+    // console.log("increment");
+    // this.state.counter++;
+    this.setState({
+      counter: this.state.counter + 1,
+    });
+  }
 }
 
 export default Counter;
